bcd001a729b6d657fe2654b2263e06e26c737eaa
Dinh Van Khang <joseph.dinhkhang@gmail.com>
2017-08-23 15:31:25 +0700

    Creat simple Api

---
+++ Trymsunsoan/Hello/Model/Hello.php
@@ -0,0 +1,18 @@
+<?php
+namespace Trymsunsoan\Hello\Model;
+use Trymsunsoan\Hello\Api\HelloInterface;
+
+class Hello implements HelloInterface
+{
+    /**
+     * Returns greeting message to user
+     *
+     * @api
+     * @param string $name Users name.
+     * @return string Greeting message with users name.
+     */
+    public function name($name) {
+        return "Hello, " . $name;
+    }
+}
+
---
+++ Trymsunsoan/Hello/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'Trymsunsoan_Hello',
+        __DIR__
+ );
